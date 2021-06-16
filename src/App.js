eccbdf903f094ffe3190ead35b99f5b57fcdba64
Abdisalan <abdisalanvp77@gmail.com>
2021-06-16 01:44:48 -0400

    added the spinner and alerts

--- src/App.js
+++ src/App.js
@@ -1,17 +1,79 @@
+import axios from 'axios';
 import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import './App.css';
+import { Alert } from './components/layout/Alert';
 import Navbar from './components/layout/Navbar';
+import About from './components/pages/About';
+import Search from './components/users/Search';
 import Users from './components/users/Users';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+    alert: null,
+  };
+
+  // async componentDidMount() {
+  //   this.setState({ loading: true });
+  //   const res = await axios.get(
+  //     `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+  //   );
+  //   this.setState({ users: res.data, loading: false });
+  // }
+
+  //search github user
+  searchUsers = async (text) => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ users: res.data.items, loading: false });
+  };
+
+  //Clear users from state
+  clearUsers = () => this.setState({ users: [], loading: false });
+
+  //set alert to display when user searches empty
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+
+    setTimeout(() => this.setState({ alert: null }), 5000);
+  };
+
   render() {
+    const { users, loading } = this.state; //destructure
     return (
-      <Fragment>
-        <Navbar />
-        <div className='container'>
-          <Users />
-        </div>
-      </Fragment>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <div className='container'>
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route
+                exact
+                path='/'
+                render={(props) => (
+                  <Fragment>
+                    {/* searchuser and clearuser props came up while showclear is going down */}
+                    <Search
+                      searchUsers={this.searchUsers}
+                      clearUsers={this.clearUsers}
+                      showClear={users.length > 0 ? true : false}
+                      setAlert={this.setAlert}
+                    />
+                    <Users loading={loading} users={users} />
+                  </Fragment>
+                )}
+              />
+
+              <Route exact path='/about' component={About} />
+            </Switch>
+          </div>
+        </Fragment>
+      </Router>
     );
   }
 }
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,39 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
+import Spinner from '../layout/Spinner';
 import UserItem from './UserItem';
+import PropTypes from 'prop-types';
 
-export class Users extends Component {
-  state = {
-    users: [
-      {
-        id: 'id',
-        login: 'mjombo',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-        html_url: 'https://github.com/mojombo',
-      },
-      {
-        id: 'id2',
-        login: 'mjombo',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-        html_url: 'https://github.com/mojombo',
-      },
-      {
-        id: 'id3',
-        login: 'mjombo',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-        html_url: 'https://github.com/mojombo',
-      },
-    ],
-  };
-  render() {
+const Users = ({ users, loading }) => {
+  if (loading) {
+    return <Spinner />;
+  } else {
     return (
       <div style={userStyle}>
-        {this.state.users.map((user) => {
+        {users.map((user) => {
           return <UserItem key={user.id} user={user} />;
         })}
       </div>
     );
   }
-}
+};
+
+Users.propTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired,
+};
 
 const userStyle = {
   display: 'grid',
